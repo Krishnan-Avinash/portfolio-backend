93df1b2bac16c72aa83379aac8c4168a8e6531d8
Avinash <avinash.krishnan1605@gmail.com>
2024-12-21 12:39:52 +0530

    Added python script for web scraping gfg profile"

--- controller/coding.controller.js
+++ controller/coding.controller.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { exec } from "child_process";
+
 export const getLeetcode = async (req, res) => {
   try {
     const resp = await axios.get(process.env.LEETCODE);
@@ -9,10 +11,17 @@ export const getLeetcode = async (req, res) => {
 };
 
 export const getGFG = async (req, res) => {
-  try {
-    const resp = await axios.get(process.env.GFG);
-    return res.status(200).send({ message: "Successful", data: resp.data });
-  } catch (error) {
-    return res.status(500).send({ message: "Some error occured" });
-  }
+  exec("python python-scripts/main.py", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing Python script: ${error.message}`);
+      return res.status(500).json({ error: "Error executing Python script" });
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return res.status(500).json({ error: stderr });
+    }
+    console.log(`stdout: ${stdout}`);
+    // Send stdout as a response to the client
+    res.json({ output: stdout });
+  });
 };
---
+++ python-scripts/main.py
@@ -0,0 +1,31 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_data():
+    url="https://www.geeksforgeeks.org/user/avinashkriad3q/"
+    r=requests.get(url)
+    htmlContent=r.content
+    soup=BeautifulSoup(htmlContent,'html.parser')
+    text=soup.find_all('script',id="__NEXT_DATA__")[0].get_text()
+    data=json.loads(text)
+    total_problems_solved=data["props"]["pageProps"]["userInfo"]["total_problems_solved"]
+    easyCount=0
+    easy=data["props"]["pageProps"]["userSubmissionsInfo"]["Easy"]
+    medium=data["props"]["pageProps"]["userSubmissionsInfo"]["Medium"]
+    hard=data["props"]["pageProps"]["userSubmissionsInfo"]["Hard"]
+    easyCount=len(easy)
+    mediumCount=len(medium)
+    hardCount=len(hard)
+    objectToReturn={
+        "totalSolved":total_problems_solved,
+        "easySolved":easyCount,
+        "mediumSolved":mediumCount,
+        "hardSolved":hardCount
+    }
+    return objectToReturn
+
+if __name__ == "__main__":
+    result = get_data()
+    print("Returned Object:", result)
