011dcef23fa81052858a0f03c903cbd0e6b2e78e
Krishnan Avinash <avinash.krishnan1605@gmail.com>
2026-01-04 17:01:20 +0530

    Fixed gfg fetch api

--- python-scripts/main.py
+++ python-scripts/main.py
@@ -8,16 +8,49 @@ def get_data():
     r=requests.get(url)
     htmlContent=r.content
     soup=BeautifulSoup(htmlContent,'html.parser')
-    text=soup.find_all('script',id="__NEXT_DATA__")[0].get_text()
-    data=json.loads(text)
-    total_problems_solved=data["props"]["pageProps"]["userInfo"]["total_problems_solved"]
+    text=soup.find_all('script')
+
+    key = 'userData'
+
+    start = text.find(key)
+    # print(start)
+    if start == -1:
+        raise ValueError("userData not found")
+
+    # move to the opening {
+    start = start + len(key)
+
+    # skip whitespace if any
+    while text[start] != '{':
+        start += 1
+
+    brace_count = 1
+    end = start + 1
+
+    for i in range(start + 1, len(text)):
+        if text[i] == '{':
+            brace_count += 1
+        elif text[i] == '}':
+            brace_count -= 1
+            if brace_count == 0:
+                end = i + 1
+                break
+
+    user_data_text = text[start:end]
+
+    # print("USER DATA TEXT:", user_data_text)
+    user_data_text = json.loads(f'"{user_data_text}"')
+
+    data = json.loads(user_data_text)
+    total_problems_solved=data["data"]["total_problems_solved"]
+
     easyCount=0
-    easy=data["props"]["pageProps"]["userSubmissionsInfo"]["Easy"]
-    medium=data["props"]["pageProps"]["userSubmissionsInfo"]["Medium"]
-    hard=data["props"]["pageProps"]["userSubmissionsInfo"]["Hard"]
-    easyCount=len(easy)
-    mediumCount=len(medium)
-    hardCount=len(hard)
+    # easy=data["props"]["pageProps"]["userSubmissionsInfo"]["Easy"]
+    # medium=data["props"]["pageProps"]["userSubmissionsInfo"]["Medium"]
+    # hard=data["props"]["pageProps"]["userSubmissionsInfo"]["Hard"]
+    easyCount=-1
+    mediumCount=-1
+    hardCount=-1
     objectToReturn={
         "totalSolved":total_problems_solved,
         "easySolved":easyCount,
